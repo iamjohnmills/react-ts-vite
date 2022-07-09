3778648a4bc29da521c4189170d6ac86f8709350
John Mills <jpm23456@gmail.com>
2022-07-09 15:44:23 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import EventBus from "./EventBus";
+
+interface IAppProps {}
+
+interface IAppState {
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+	constructor(props: IAppProps) {
+		super(props);
+		this.state = {
+		};
+	}
+	componentDidMount(): void {
+	}
+	render() { return (
+  <div className="App">
+  </div>
+	) }
+}
+
+export default App
