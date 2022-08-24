454f642a1d6f4e40299ae334ded685c471eb1fc6
John <john@Johns-MacBook-Pro.local>
2022-08-24 17:01:16 -0500

    added interactive, fun symbol chooser

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,36 @@ import EventBus from "./EventBus";
 
 interface IAppProps {}
 
-interface IAppState {}
+interface IAppState {
+  symbol: string|null
+}
 
 class App extends React.Component<IAppProps, IAppState> {
+  private some_symbols_for_ya:Array<string> = ['!','@','Whoa','#','$','%','&','*','Cool!'];
   constructor(props: IAppProps) {
     super(props);
-    this.state = {};
+    this.state = {
+      symbol: null
+    };
   }
   componentDidMount(): void {
+    this.setRandomSymbol();
   }
-  render() { return (
-  <div className="App">
-  HELLO WORLD
+  setRandomSymbol(): void {
+    const rando:string = this.some_symbols_for_ya[Math.floor(Math.random()*this.some_symbols_for_ya.length)]
+    if(rando !== this.state.symbol){
+      this.setState({ symbol: rando });
+    } else {
+      this.setRandomSymbol();
+    }
+  }
+  handleSymbolClick(): void {
+    this.setRandomSymbol();
+  }
+  render(): React.ReactNode { return (
+  <div className="App center cursor-pointer user-select-none" onClick={this.handleSymbolClick.bind(this)}>
+    <div>HELLO WORLD!</div>
+    <div>{this.state.symbol}</div>
   </div>
   ) }
 }
